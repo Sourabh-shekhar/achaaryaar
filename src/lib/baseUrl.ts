46e042edc019d897aea5d54e97a4b09613d7a2ff
Sourabh Shekhar <SOURABHSHEKHAR0146@GMAIL.COM>
2026-07-07 11:10:18 +0530

    Update Inventory page

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,4 +1,4 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://achaaryaar.vercel.app"
+    ? process.env.NEXT_PUBLIC_SITE_URL || "https://achaaryaar.com"
     : "http://localhost:3000";
